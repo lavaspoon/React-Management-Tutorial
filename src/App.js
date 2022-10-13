f2d27eed0d5a49b5746bbd28ec0aa829b1e6ae3f
lavaspoon <lavaspoon@naver.com>
2022-10-13 22:43:29 +0900

    컴포넌트 개발

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = {
+  'name': '이은호',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
